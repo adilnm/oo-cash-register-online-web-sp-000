fc8dc044409293868813ed87e317217f0e40d446
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-09 09:20:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ def add_item(title,price,quantity=nil)
   if(quantity)
   new_price=price*quantity
   self.total+=new_price
-  @allarray.fill(12, array.size, 4)
+  @all.fill(title, @all.size, quantity)
 else
   self.total+=price
   @all << title
