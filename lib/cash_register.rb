222a0798728ede35850d3d7fb833ec45828c78a1
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-09 08:45:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,9 @@ class CashRegister
   
   attr_accessor :total, :discount
   
-  def initialize
+  def initialize(discount=nil)
   @total=0
-  @discount=20
+  @discount=discount
 end
 
 def add_item(title,price)
